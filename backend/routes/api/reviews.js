1e4b198b530441a879d34d4c38fd6e82fde8f930
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-04-18 16:19:28 -0400

    almost done with the get all reviews from current user

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -2,32 +2,46 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Review } = require('../../db/models');
+const { Review, Spot, SpotImage, ReviewImage, User} = require('../../db/models');
 
-router.post('/:spotId/review', async (req, res) => {
-    const { user } = req
-    const spotId = req.params.spotId;
-    const userId = user.id;
-    const { review, stars } = req.body;
 
-    if (!user) {
-        return res.status(403).json({
-            message: "Forbidden"
-        })
-    }
-
-    const newReview = await Review.create({ userId, spotId, review, stars})
-
-})
 router.get('/current', async (req, res) => {
+    const { user } = req;
+    if (!user) {
+        return res.status(403).json({message: "Forbidden"})
+    }
+    const reviews = await Review.findAll({
+        where: {
+            userId: user.id
+        },
+        include: [
+            {
+                model: User,
+                attributes : ['id','firstName', 'lastName']
+            },
+            {
+                model: Spot,
+                attributes: {
+                    exclude: ['createdAt', 'updatedAt']
+                }
+            },
+            {
+                model: ReviewImage
+            }
+        ]
+    })
 
+    res.json({ Reviews: reviews })
 })
+
+
+
+
 router.get('', async (req, res) => {
     const allReviews = await Review.findAll({
         order: [['id']]
     })
-
-    res.json(allReviews)
+    res.json({Reviews : allReviews})
 })
 
 
