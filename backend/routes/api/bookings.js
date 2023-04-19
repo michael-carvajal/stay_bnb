47034f4b270cef3c8306f383c389d0bf27a3fd14
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-04-19 15:42:37 -0400

    completed get all bookings based on current user

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -2,12 +2,64 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Booking } = require('../../db/models');
+const { Booking , Spot, SpotImage} = require('../../db/models');
 
 router.get('',async(req, res) => {
     const allBookings = await Booking.findAll();
 
     res.json(allBookings)
+})
+router.get('/spotImages/spot',async(req, res) => {
+   try {
+     const url = await SpotImage.findAll({
+         where: {
+
+            spotId: 1
+
+         },
+
+     })
+     res.json(url)
+   } catch (error) {
+        res.json(error)
+   }
+})
+router.get('/current', requireAuth, async (req, res) => {
+    const { user } = req;
+
+    const allBookings = await Booking.findAll({
+        where: {
+            userId: user.id
+        },
+        include: {
+            model: Spot,
+            attributes: {
+                exclude: ['createdAt', 'updatedAt']
+            }
+        }
+    });
+
+    try {
+        const bookingsWithPreview = await Promise.all(allBookings.map( async booking => {
+            const bookingObj = booking.toJSON();
+            console.log(bookingObj.spotId);
+
+            const url = await SpotImage.findOne({
+                where: {
+                    spotId: bookingObj.spotId,
+
+                },
+                attributes: ['url']
+            })
+            const urlObj = url.toJSON()
+            bookingObj.Spot.previewImage = urlObj.url
+            return bookingObj
+        }))
+
+        res.json(bookingsWithPreview)
+    } catch (error) {
+        res.status(400).json(error)
+    }
 })
 
 
