4501378af1a64c904e024e59f9a80907fb82ea54
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-05-05 13:01:55 -0400

    added login page

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,3 +1,80 @@
+/*
+## AirBnb Store Shape:
+
+```js
+store = {
+  session: {},
+  spots: {
+    // Notice there are two slices of state within spots. This is to handle your two different routes for getting a spot.
+    // Refer to your API Docs to get more information.
+    allSpots: {
+      [spotId]: {
+        spotData,
+      },
+      // These optional ordered lists are for you to be able to store an order in which you want your data displayed.
+      // you can do this on the frontend instead of in your slice is state which is why it is optional.
+      optionalOrderedList: [],
+    },
+    // Notice singleSpot has more data that the allSpots slice. Review your API Docs for more information.
+    singleSpot: {
+      spotData,
+      SpotImages: [imagesData],
+      Owner: {
+        ownerData,
+      },
+    },
+  },
+  // Again the idea here is two have separate slices for the different data responses you receive from your routes.
+  // For example, you could use each of these slices specifically for the component you are dealing with on the frontend.
+  reviews: {
+    // When on a single spot, use the spot slice.
+    spot: {
+      [reviewId]: {
+        reviewData,
+        User: {
+          userData,
+        },
+        ReviewImages: [imagesData],
+      },
+      optionalOrderedList: [],
+    },
+    // When on the user's reviews, use the user slice.
+    user: {
+      [reviewId]: {
+        reviewData,
+        User: {
+          userData,
+        },
+        Spot: {
+          spotData,
+        },
+        ReviewImages: [imagesData],
+      },
+      optionalOrderedList: [],
+    },
+  },
+  bookings: {
+    user: {
+      [bookingId]: {
+        bookingData,
+        Spot: {
+          spotData,
+        },
+      },
+      optionalOrderedList: [],
+    },
+    // Note here that your responses can actually be different here as well.
+    // HINT: What information should you see if you own this spot? (Refer to API Docs).
+    spot: {
+      [bookingId]: {
+        bookingData,
+      },
+      optionalOrderedList: [],
+    },
+  },
+};
+```
+*/
 import { csrfFetch } from "./csrf"
 const LOG_IN = "session/LOG_IN"
 const LOG_OUT = "session/LOG_OUT"
@@ -8,35 +85,41 @@ const logIn = (user) => {
         user
     }
 }
-const logOut = (user) => {
+const logOut = () => {
     return {
-        type: LOG_OUT,
-        user
+        type: LOG_OUT
     }
 }
 
 export const  sessionLogIn = (user) => async dispatch => {
 
 
-    console.log(1);
-    console.log(user);
+    const { credential, password } = user;
     const response = await csrfFetch('/api/session', {
         method: "POST",
         headers: { "Content-Type": 'application/json' },
-        body: JSON.stringify(user)
+        body: JSON.stringify({
+            credential,
+            password,
+        })
     })
-    console.log(2);
+
 
     if (response.ok) {
         const signedInUser = await response.json();
         return dispatch(logIn(signedInUser))
     } else {
         const error = await response.json();
-        console.log(2);
 
         console.log(error);
     }
 }
+export const restoreUser = () => async (dispatch) => {
+    const response = await csrfFetch("/api/session");
+    const data = await response.json();
+    dispatch(logIn(data.user));
+    return response;
+};
 
 
 const initialState = { "user" : null}
