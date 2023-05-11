15af3b3001cae6d0c90b900deca8f95b74e755d7
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-05-10 21:09:12 -0400

    added switch design rto log out btn

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -53,8 +53,8 @@ function ProfileButton({ user }) {
                         <li>{user.firstName} {user.lastName}</li>
                         <li>{user.email}</li>
                         <li><NavLink to="/spots/current">Manage Spots</NavLink></li>
-                        <li>
-                            <button onClick={logout}>Log Out</button>
+                        <li className="logout-btn" >
+                            <p onClick={logout}>Log Out</p>
                         </li>
                     </>
                 ) : (
