fe0d858abaed57eada7b8f8aef5a72b91fc948c0
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-05-12 07:27:43 -0400

    new to when there are no reviews on spot in allSpots

--- frontend/src/components/ShowSpots/index.js
+++ frontend/src/components/ShowSpots/index.js
@@ -25,13 +25,14 @@ export const ShowSpots = () => {
             {allSpots?.map((spot, index) => {
                 // console.log(spot?.id);
                 const roundedAvg = spot?.avgRating
+                const rating = spot?.avgRating === null ? <p className="normal-font">New</p> : <p className="normal-font">{roundedAvg}</p>;
                 return (
                     <NavLink key={spot.id} className="spot-card" to={`/spots/${spot?.id}` }>
                         <img src={spot?.previewImage} alt={spot?.name} className="preview-image" onError={(e) => { e.target.onerror = null; e.target.src = missingImage; }} />
 
                         <div className="location-rating ">
                             <p className="normal-font">{spot?.city}, {spot?.state}</p>
-                            <p className="normal-font">{roundedAvg}</p>
+                        {rating}
                         </div>
                         <div className="normal-font"><p className="reserve-price"> ${spot?.price}</p> night</div>
 
